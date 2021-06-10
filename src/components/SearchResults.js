f2f6bc8b649f5c3b206196b280a190fa2ef25835
rgmantle <graig.mantle@gmail.com>
2021-06-09 21:32:05 -0700

    Part 02 almost complete

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,10 +1,16 @@
 
 import React from 'react';
 
+import Card from './Card'
+
 const SearchResults = ({ results }) => {
   return (
     <div id="results">
       <h3>Here's what we pulled up ({ results.length } results):</h3>
+      { results.map (result => (
+        <Card key={ result.id } { ...result } />
+      ))
+      }
     </div>
   );
 }
